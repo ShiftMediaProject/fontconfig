3dd2c547356b823bea1252f1139bba59f0092e88
Matt Oliver <protogonoi@gmail.com>
2025-06-15 23:43:50 +1000

    project: Update for 2.16.2 merge.

--- SMP/src/fcstdint.h
+++ SMP/src/fcstdint.h
@@ -1,7 +1,7 @@
 #ifndef _FONTCONFIG_SRC_FCSTDINT_H
 #define _FONTCONFIG_SRC_FCSTDINT_H 1
 #ifndef _GENERATED_STDINT_H
-#define _GENERATED_STDINT_H "fontconfig 2.16.1"
+#define _GENERATED_STDINT_H "fontconfig 2.16.2"
 #define _STDINT_HAVE_STDINT_H 1
 #include <stdint.h>
 #endif
